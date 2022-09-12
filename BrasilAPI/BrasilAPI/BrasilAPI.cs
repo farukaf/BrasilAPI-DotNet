1b5bdbc77c0162dbeff0dcee74194a58db289d43
Faruk Abdo Feres <faruk.feres@mottu.com.br>
2022-09-11 21:13:04 -0300

    [feat] Adicionado Registro BR endpoint

--- BrasilAPI/BrasilAPI/BrasilAPI.cs
+++ BrasilAPI/BrasilAPI/BrasilAPI.cs
@@ -1,4 +1,5 @@
 ﻿using Newtonsoft.Json;
+using SDKBrasilAPI.Responses;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -318,7 +319,7 @@ namespace SDKBrasilAPI
         /// <returns></returns>
         public async Task<FIPEMarcasResponse> FIPE_Marcas(TipoVeiculo? tipoVeiculo = null, long? tabelaReferencia = null)
         {
-            string baseUrl = $"{BASE_URL}/fipe/marcas/v1/{tipoVeiculo?.ToString() ?? "" }";
+            string baseUrl = $"{BASE_URL}/fipe/marcas/v1/{tipoVeiculo?.ToString() ?? ""}";
 
             if (tabelaReferencia != null)
             {
@@ -396,6 +397,25 @@ namespace SDKBrasilAPI
             return fipeResponse;
         }
 
+        /// <summary>
+        /// Avalia o status de um dominio .br
+        /// </summary>
+        /// <param name="dominio">O domínio ou nome a ser avaliado</param>
+        /// <returns></returns>
+        public async Task<RegistroBrResponse> RegistroBR(string dominio)
+        {
+            string baseUrl = $"{BASE_URL}/registrobr/v1/{dominio.Trim()}";
+            var response = await Client.GetAsync(baseUrl);
+            await EnsureSuccess(response, baseUrl);
+            var json = await response.Content.ReadAsStringAsync();
+
+            var registroResponse = JsonConvert.DeserializeObject<RegistroBrResponse>(json);
+
+            registroResponse.CalledURL = baseUrl;
+            registroResponse.JsonResponse = json;
+
+            return registroResponse;
+        }
 
 
         private const string BASE_URL = "https://brasilapi.com.br/api";
---
+++ BrasilAPI/BrasilAPI/Responses/RegistroBrResponse.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Runtime.Serialization;
+using System.Text;
+
+namespace SDKBrasilAPI.Responses
+{
+    [DataContract]
+    public class RegistroBrResponse : BaseResponse
+    {
+        [DataMember(Name = "status_code")]
+        public int StatusCode { get; set; }
+        [DataMember(Name = "status")]
+        public string Status { get; set; }
+        [DataMember(Name = "fqdn")]
+        public string Fqdn { get; set; }
+        [DataMember(Name = "fqdnace")]
+        public string Fqdnace { get; set; }
+        [DataMember(Name = "exempt")]
+        public bool Exempt { get; set; }
+        [DataMember(Name = "hosts")]
+        public string[] Hosts { get; set; }
+        [DataMember(Name = "publicationstatus")]
+        public string PublicationStatus { get; set; }
+        [DataMember(Name = "expiresat")]
+        public DateTime ExpiresAt { get; set; }
+        [DataMember(Name = "suggestions")]
+        public string[] Suggestions { get; set; }
+    }
+}
