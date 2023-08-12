0fa3f15907f2b0922c4a1026fb4701ff826389be
Faruk Abdo Feres <faruk@mottu.com.br>
2023-08-12 17:11:49 -0300

    [test] added test on ncm

---
+++ BrasilAPI/BrasilAPI.Test/NCM_Test.cs
@@ -0,0 +1,54 @@
+﻿using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SDKBrasilAPI; 
+using System.Linq; 
+using System.Threading.Tasks;
+
+namespace BrasilAPI_Test
+{
+    [TestClass]
+    public class NCM_Test
+    {
+        [TestMethod]
+        public async Task Test01()
+        {
+            //Arrange 
+
+            //Act
+            using var brasilAPI = new BrasilAPI();
+            var corretorasResponse = await brasilAPI.NCM();
+
+            //Assert
+            Assert.IsNotNull(corretorasResponse);
+            Assert.IsTrue(corretorasResponse.NCMs.Any());
+        }
+
+        [TestMethod]
+        public async Task Test02()
+        {
+            //Arrange 
+
+            //Act
+            using var brasilAPI = new BrasilAPI();
+            var corretorasResponse = await brasilAPI.NCM_Busca("33");
+
+            //Assert
+            Assert.IsNotNull(corretorasResponse);
+            Assert.IsTrue(corretorasResponse.NCMs.Any());
+        }
+
+        [TestMethod]
+        public async Task Test03()
+        {
+            //Arrange 
+
+            //Act
+            using var brasilAPI = new BrasilAPI();
+            var corretorasResponse = await brasilAPI.NCM("3305.10.00");
+
+            //Assert
+            Assert.IsNotNull(corretorasResponse);
+            Assert.IsTrue(corretorasResponse.NCMs.Any());
+            Assert.IsTrue(corretorasResponse.NCMs.Count() == 1);
+        }
+    }
+}
