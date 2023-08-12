c11c2c264d090a1fe440dcf477ffc3cbe0dcdbe0
Faruk Abdo Feres <faruk@mottu.com.br>
2023-08-12 17:26:44 -0300

    [feat] added pix and pix test

---
+++ BrasilAPI/BrasilAPI/BrasilAPI_PIX.cs
@@ -0,0 +1,37 @@
+﻿using Newtonsoft.Json;
+using SDKBrasilAPI.Responses;
+using System;
+using System.Collections.Generic;
+using System.Threading.Tasks;
+
+
+namespace SDKBrasilAPI
+{
+    public partial class BrasilAPI : IDisposable
+    {
+        /// <summary>
+        /// Retorna informações de todos os participantes do PIX no dia atual ou anterior
+        /// </summary>
+        /// <returns></returns>
+        public async Task<ParticipantePIXResponse> ParticipantesPIX()
+        {
+            string baseUrl = $"{BASE_URL}/pix/v1/participants";
+
+            var httpResponse = await Client.GetAsync(baseUrl);
+
+            await EnsureSuccess(httpResponse, baseUrl);
+
+            var json = await httpResponse.Content.ReadAsStringAsync();
+
+            var response = new ParticipantePIXResponse()
+            {
+                Parcipantes = JsonConvert.DeserializeObject<IEnumerable<ParticipantePIX>>(json),
+                CalledURL = baseUrl,
+                JsonResponse = json
+            };
+
+            return response;
+        }
+         
+    }
+}
